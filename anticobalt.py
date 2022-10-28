e3da64dd4487ee3afbe41645512930e054593f69
Ali Can Gönüllü <alicangonullu@yahoo.com>
2022-10-28 20:31:55 +0300

    Create anticobalt.py

---
+++ anticobalt.py
@@ -0,0 +1,41 @@
+import shodan, os, platform, csv
+API_KEY = "SHODAN_API"
+
+class Color():
+    @staticmethod
+    def red(str):
+        return "\033[91m" + str + "\033[0m"
+    @staticmethod
+    def green(str):
+        return "\033[92m" + str + "\033[0m"
+    @staticmethod
+    def yellow(str):
+        return "\033[93m" + str + "\033[0m"
+    @staticmethod
+    def blue(str):
+        return "\033[94m" + str + "\033[0m"
+
+def shodan_cobalt(SHODAN, searchkey):
+    try:
+        api = shodan.Shodan(SHODAN)
+        results = api.search(str(searchkey))
+        for result in results['matches']:
+            ip = format(result['ip_str'])
+            if(platform.system() == "Windows"):
+                print(Color.red("Windows are not supporting!"))
+            else:
+                print(Color.red(str(ip) + " IP adresi engellendi!\n"))
+                os.system('iptables -A INPUT -s ' + str(ip) + ' -j DROP')
+                os.system('iptables -A OUTPUT -d ' + str(ip) + ' -j DROP')
+                data = [str(searchkey), str(ip)]
+                with open('ipttable.csv', 'w') as file:
+                    writer = csv.writer(file)
+                    writer.writerow(data)
+    except:
+        print(Color.red("HARD ERROR"))
+
+
+
+arraysearch = ['product:"Cobalt Strike Beacon"', 'product:"Cobalt"']
+for search in arraysearch:
+    shodan_cobalt(API_KEY, search)
